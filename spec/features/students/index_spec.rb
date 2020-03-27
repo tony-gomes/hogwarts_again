4ebf5f53e9e8194917c282effbae42f43603dbc1
Anthony Gomes <code@tonygom.es>
2020-03-27 10:42:32 -0600

    Add US2 + some US3 by accident

---
+++ spec/features/students/index_spec.rb
@@ -0,0 +1,34 @@
+require 'rails_helper'
+
+RSpec.describe 'As a visitor' do
+  it 'when I visit /students I see a list of courses & number of professors per student' do
+
+    snape = Professor.create(name: "Severus Snape", age: 45, specialty: "Potions")
+    hagarid = Professor.create(name: "Rubus Hagarid", age: 38 , specialty: "Care of Magical Creatures")
+    lupin = Professor.create(name: "Remus Lupin", age: 49 , specialty: "Defense Against The Dark Arts")
+
+    harry = Student.create(name: "Harry Potter" , age: 11 , house: "Gryffindor" )
+    malfoy = Student.create(name: "Draco Malfoy" , age: 12 , house: "Slytherin" )
+    longbottom = Student.create(name: "Neville Longbottom" , age: 11 , house: "Gryffindor" )
+
+    ProfessorStudent.create(student_id: harry.id, professor_id: snape.id)
+    ProfessorStudent.create(student_id: harry.id, professor_id: hagarid.id)
+    ProfessorStudent.create(student_id: harry.id, professor_id: lupin.id)
+    ProfessorStudent.create(student_id: malfoy.id, professor_id: hagarid.id)
+    ProfessorStudent.create(student_id: malfoy.id, professor_id: lupin.id)
+    ProfessorStudent.create(student_id: longbottom.id, professor_id: snape.id)
+
+    visit '/students'
+
+    expect(current_path).to eq("/students")
+
+    page.has_content?(harry.name)
+    # page.has_content?(harry.name)
+
+    page.has_content?(hagarid.name)
+    # page.has_content?(hagarid.name)
+
+    page.has_content?(lupin.name)
+    # page.has_content?(lupin.name)
+  end
+end
